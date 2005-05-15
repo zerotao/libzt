3d90b53e2edfb298af4dd15b73c8961393924d6a
jshiffer <jshiffer@e6656917-31e5-0310-b941-e28658107539>
2005-05-15 12:28:00 +0000

    removed zt_bstream class

--- libzt/zt_bstream/zt_bstream_interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ZT_STREAM_INTERFACE_H_
-#define _ZT_STREAM_INTERFACE_H_
-
-#include <libzt/zt_bstream.h>
-
-#include <libzt/zt_bstream/zt_bstream_file.h>
-#include <libzt/zt_bstream/zt_bstream_socket.h>
-#endif  /* _ZT_STREAM_INTERFACE_H_ */
