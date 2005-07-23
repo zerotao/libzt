1b2114a197ccb6a77243cebcc8200aa715f27b14
jshiffer <jshiffer@e6656917-31e5-0310-b941-e28658107539>
2005-07-23 20:33:03 +0000

    fixed a bug in cfg_test fixed Makefile.am so that is test failures are detected

--- libzt/test/cfg_test.c
+++ libzt/test/cfg_test.c
@@ -1,7 +1,7 @@
 /*
  * cfg_test.c        config file parser test
  *
- * Copyright (C) 2000-2002, 2004, Jason L. Shiffer <jshiffer@zerotao.com>.  All Rights Reserved.
+ * Copyright (C) 2000-2002, 2004, 2005, Jason L. Shiffer <jshiffer@zerotao.com>.  All Rights Reserved.
  * See file COPYING for details.
  *
  * $Id: cfg_test.c,v 1.2 2003/06/09 13:42:12 jshiffer Exp $
@@ -25,8 +25,16 @@ main ( argc, argv )
   char *s = NULL;
   double f = 0.0, f2 = 0.00, f3 = 0.00;
   short int b = 0;
+  int n = 0;
   
   cfg = cfg_ini("cfg_test.ini", 0);
+  
+  TEST_N("cfg", n, (cfg != NULL));
+  if(!cfg) {
+	  printf("Could not open file cfg_test.ini\n");
+	  exit(1);
+  }
+  
 
 
   cfg_get(cfg, "main", "bool_var", &b, cfg_bool);
@@ -34,10 +42,10 @@ main ( argc, argv )
   cfg_get(cfg, "main", "float_var", &f, cfg_float);
   cfg_get(cfg, "main", "string_var", &s, cfg_string);
 
-  TEST("cfg[0]: ", (b == 1));
-  TEST("cfg[1]: ", (i == 1));
-  TEST("cfg[3]: ", (f == 99.999));
-  TEST("cfg[4]: ", (!strcmp(s, "This is a string")));
+  TEST_N("cfg", n, (b == 1));
+  TEST_N("cfg", n, (i == 1));
+  TEST_N("cfg", n, (f == 99.999));
+  TEST_N("cfg", n, (!strcmp(s, "This is a string")));
 
   f = f + 100.00;
   
@@ -45,9 +53,9 @@ main ( argc, argv )
   cfg_get(cfg, "main", "float_var", &f, cfg_float);
   cfg_get(cfg, "main2", "float_var", &f2, cfg_float);
   cfg_get(cfg, "main3", "float_var", &f3, cfg_float);
-  TEST("cfg[5]: ", (f == 199.999));
-  TEST("cfg[6]: ", (f2 == 199.999));
-  TEST("cfg[7]: ", (f3 == 99.999));      
+  TEST_N("cfg", n, (f == 199.999));
+  TEST_N("cfg", n, (f2 == 199.999));
+  TEST_N("cfg", n, (f3 == 99.999));      
   
   cfg_close(cfg);
   return 0;
