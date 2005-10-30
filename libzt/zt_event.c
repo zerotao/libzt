3bcd4b5a23848e4daced29ecd899d07dffbabbbd
jshiffer <jshiffer@e6656917-31e5-0310-b941-e28658107539>
2005-10-30 11:09:52 +0000

    event_timers   added new argument to zt_event_register_timer zt_event_timer_flags   defined the timer flags
    new functions
      zt_event_num_events
        which returnes the number of events currently in the event handler
    
      zt_event_tod
        As we need to get the timeofday often I cash it here and this
        function returns a pointer to it
    
    new definitions
      ZT_ALL_IO_EVENTS
      ZT_ANY_IO_EVENT
      ZT_ANY_EVENT

--- libzt/zt_event.c
+++ libzt/zt_event.c
@@ -6,9 +6,13 @@
 
 int zt_event_register_io(zt_event_sys sys, int fd, zt_event_enum type, zt_event_io_cb cb, void *data)
 {
+	int	  ret;
 	assert(sys);
 	if(sys->vtbl->register_io){
-		return sys->vtbl->register_io(sys, fd, type, cb, data);
+		if((ret = sys->vtbl->register_io(sys, fd, type, cb, data)) == 0) {
+			sys->num_events++;
+		}
+		return ret;
 	}
 	
 	return -1;
@@ -16,9 +20,14 @@ int zt_event_register_io(zt_event_sys sys, int fd, zt_event_enum type, zt_event_
 
 int zt_event_remove_io(zt_event_sys sys, int fd, zt_event_enum type)
 {
+	int	  ret = 0;
+	
 	assert(sys);
 	if(sys->vtbl->remove_io){
-		return sys->vtbl->remove_io(sys, fd, type);
+		if((ret = sys->vtbl->remove_io(sys, fd, type)) == 0){
+			sys->num_events--;
+		}
+		return ret;
 	}
 	
 	return -1;
@@ -27,9 +36,14 @@ int zt_event_remove_io(zt_event_sys sys, int fd, zt_event_enum type)
 
 int zt_event_register_signal(zt_event_sys sys, int signal, zt_event_signal_cb cb, void *data)
 {
+	int	  ret = 0;
+	
 	assert(sys);
 	if(sys->vtbl->register_signal){
-		return sys->vtbl->register_signal(sys, signal, cb, data);
+		if((ret = sys->vtbl->register_signal(sys, signal, cb, data)) == 0){
+			sys->num_events++;
+		}
+		return ret;
 	}
 	
 	return -1;
@@ -37,20 +51,31 @@ int zt_event_register_signal(zt_event_sys sys, int signal, zt_event_signal_cb cb
 
 int zt_event_remove_signal(zt_event_sys sys, int signal, zt_event_signal_cb cb, void *data)
 {
+	int	  ret = 0;
+	
 	assert(sys);
 	if(sys->vtbl->remove_signal){
-		return sys->vtbl->remove_signal(sys, signal, cb, data);
+		if((ret = sys->vtbl->remove_signal(sys, signal, cb, data)) == 0){
+			sys->num_events--;
+		}
+		return ret;
 	}
 	
 	return -1;
 }
 
 
-int zt_event_register_timer(zt_event_sys sys, struct timeval *time, zt_event_timer_cb cb, void *data)
+int zt_event_register_timer(zt_event_sys sys, struct timeval *time, zt_event_timer_cb cb, void *data, zt_event_timer_flags flags)
 {
+	int	  ret = 0;
+	
 	assert(sys);
 	if(sys->vtbl->register_timer){
-		return sys->vtbl->register_timer(sys, time, cb, data);
+		if((ret = sys->vtbl->register_timer(sys, time, cb, data, flags)) == 0){
+			sys->num_events++;
+		}
+		
+		return ret;
 	}
 	
 	return -1;
@@ -58,9 +83,14 @@ int zt_event_register_timer(zt_event_sys sys, struct timeval *time, zt_event_tim
 
 int zt_event_remove_timer(zt_event_sys sys, struct timeval *time, zt_event_timer_cb cb, void *data)
 {
+	int	  ret = 0;
+	
 	assert(sys);
 	if(sys->vtbl->remove_timer){
-		return sys->vtbl->remove_timer(sys, time, cb, data);
+		if((ret = sys->vtbl->remove_timer(sys, time, cb, data)) == 0){
+			sys->num_events--;
+		}
+		return ret;
 	}
 	
 	return -1;
@@ -86,5 +116,17 @@ int zt_event_run(zt_event_sys sys, zt_event_flags flags)
 	}
 	
 	return -1;
+}
+
+long zt_event_num_events(zt_event_sys sys)
+{
+	assert(sys);
+	return sys->num_events;
+}
+
+struct timeval* zt_event_tod(zt_event_sys sys)
+{
+	assert(sys);
+	return &sys->tod;
 }
 
