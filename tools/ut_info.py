c222257de0d893f8fd6eb7196d38c0cbc9558108
Jason L. Shiffer <jshiffer@zerotao.org>
2007-12-09 23:23:34 -0500

    added ut_info.py

---
+++ tools/ut_info.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import yaml
+
+def stat_suite(suite):
+    assertions = 0
+    tests = 0
+    failures = 0
+    successes = 0
+
+    test_section = suite['Tests']
+    if test_section != None:
+        for name, values in test_section.items():
+            assertions += values['assertions']
+            tests += 1
+            if values['result'] == 'success':
+                successes += 1
+            else:
+                failures += 1
+
+    errors = suite.get('Errors', [])    
+    return tests, assertions, failures, successes, errors
+    
+    
+def stat_ut(fd):
+    stats = []
+    
+    tests = yaml.load_all(fd) 
+    for suite in tests:
+        for suite_name, values in suite.items():
+            sstats = stat_suite(values)
+            stats.append(sstats)
+
+    print_stat(stats)
+
+
+def print_stat(stats):
+    suites = len(stats)
+    assertions = 0
+    tests = 0
+    successes = 0
+    failures = 0
+    errors = []
+
+    for st, sa, sf, ss, er in stats:
+        tests += st
+        assertions += sa
+        successes += ss
+        failures += sf
+        if er:
+            errors.append(er)
+
+    result = {'Global Stats':
+                  {'results': 
+                   {'suites': suites,
+                    'tests': tests,
+                    'successes': successes,
+                    'assertions': assertions,
+                    'failures': failures},
+                   'errors': errors}}
+                        
+              
+
+    print "---"
+    print yaml.dump(result, default_flow_style=False)
+
+
+#     print """
+# ---
+# Global Stats:
+#   suites                      : %d
+#   tests                       : %d
+#   successes                   : %d
+#   assertions                  : %d
+#   failures                    : %d
+#   errors                      : %s
+# """ % (suites, tests, successes, assertions, failures, errors)
+
+
+if __name__ == '__main__':
+    import optparse
+    import sys
+
+    parser = optparse.OptionParser(usage="usage: %prog [options] file")
+
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.print_help()
+        sys.exit(1)
+
+    if args[0] == '-':
+        stat_ut(sys.stdin)
+
+    else:
+        f = open(args[0], "r")
+        stat_ut(f)
+        f.close()
