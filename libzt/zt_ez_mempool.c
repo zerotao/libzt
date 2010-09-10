ad5cb5570ab4fbfd78fd412034c46bb98d9d7829
Mark Ellzey <mark.thomas@mandiant.com>
2010-09-11 06:18:24 +0800

    Added the ability for a zt_ez_mempool_alloc to be passed an optional free() callback for that specific data

--- libzt/zt_ez_mempool.c
+++ libzt/zt_ez_mempool.c
@@ -18,14 +18,15 @@ chunk_append(zt_ez_mempool_chunk **head,
 }
 
 static zt_ez_mempool_chunk *
-zt_ez_mempool_chunk_init(const size_t size) {
+zt_ez_mempool_chunk_init(const size_t size, zt_ez_mempool_free_cb free_cb) {
     zt_ez_mempool_chunk *chunk;
 
     chunk       = malloc(sizeof(zt_ez_mempool_chunk));
 
-    chunk->sz   = size;
-    chunk->data = calloc(size, 1);
-    chunk->next = NULL;
+    chunk->sz      = size;
+    chunk->data    = calloc(size, 1);
+    chunk->free_cb = free_cb;
+    chunk->next    = NULL;
 
     return chunk;
 }
@@ -70,7 +71,13 @@ free_chunks(zt_ez_mempool_chunk *chunks) {
         zt_ez_mempool_chunk *tofree;
 
         tofree    = chunk_ptr;
-        free(chunk_ptr->data);
+
+	if (chunk_ptr->free_cb) {
+	    chunk_ptr->free_cb(chunk_ptr->data);
+	} else {
+	    free(chunk_ptr->data);
+	}
+
         chunk_ptr = chunk_ptr->next;
         free(tofree);
     }
@@ -98,10 +105,10 @@ zt_ez_mempool_destroy(zt_ez_mempool *pool) {
 }
 
 void *
-zt_ez_mempool_alloc(zt_ez_mempool *pool, size_t size) {
+zt_ez_mempool_alloc(zt_ez_mempool *pool, size_t size, zt_ez_mempool_free_cb free_cb) {
     zt_ez_mempool_chunk *chunk;
 
-    chunk = zt_ez_mempool_chunk_init(size);
+    chunk = zt_ez_mempool_chunk_init(size, free_cb);
 
     if (chunk == NULL) {
         return NULL;
@@ -113,7 +120,8 @@ zt_ez_mempool_alloc(zt_ez_mempool *pool, size_t size) {
 }
 
 int
-zt_ez_mempool_add_buffer(zt_ez_mempool *pool, void *data, size_t size) {
+zt_ez_mempool_add_buffer(zt_ez_mempool *pool, void *data, size_t size,
+	zt_ez_mempool_free_cb free_cb) {
     zt_ez_mempool_chunk *chunk;
 
     if (data == NULL || size == 0) {
@@ -125,9 +133,10 @@ zt_ez_mempool_add_buffer(zt_ez_mempool *pool, void *data, size_t size) {
         return -1;
     }
 
-    chunk->sz   = size;
-    chunk->data = data;
-    chunk->next = NULL;
+    chunk->sz      = size;
+    chunk->data    = data;
+    chunk->next    = NULL;
+    chunk->free_cb = free_cb;
 
     chunk_append(&pool->chunks, chunk);
 
@@ -135,8 +144,25 @@ zt_ez_mempool_add_buffer(zt_ez_mempool *pool, void *data, size_t size) {
 }
 
 
-#if 0
+#ifdef TEST_EZ_MEMPOOL_MAIN 
+struct test_data {
+    char *data1;
+    char *data2;
+    char *data3;
+};
+
+void test_special_free(void *data) {
+    struct test_data *tdata;
+
+   tdata = (struct test_data *)data; 
+   free(tdata->data1);
+   free(tdata->data2);
+   free(tdata->data3);
+   free(tdata);
+}
+
 int main(int argc, char **argv) {
+    struct test_data *tdata;
     zt_ez_mempool *main_pool;
     zt_ez_mempool *subpool;
     char          *data;
@@ -145,17 +171,23 @@ int main(int argc, char **argv) {
     subpool   = zt_ez_mempool_init(main_pool);
 
 
-    zt_ez_mempool_alloc(main_pool, 1024);
-    zt_ez_mempool_alloc(subpool, 1024);
-    zt_ez_mempool_alloc(subpool, 1024);
-    zt_ez_mempool_alloc(subpool, 1024);
-    zt_ez_mempool_alloc(subpool, 1024);
-    zt_ez_mempool_alloc(subpool, 1024);
+    zt_ez_mempool_alloc(main_pool, 1024, NULL);
+    zt_ez_mempool_alloc(subpool, 1024, NULL);
+    zt_ez_mempool_alloc(subpool, 1024, NULL);
+    zt_ez_mempool_alloc(subpool, 1024, NULL);
+    zt_ez_mempool_alloc(subpool, 1024, NULL);
+    zt_ez_mempool_alloc(subpool, 1024, NULL);
 
     subpool = zt_ez_mempool_init(subpool);
 
-    zt_ez_mempool_alloc(subpool, 1024);
-    zt_ez_mempool_alloc(main_pool, 1024);
+    zt_ez_mempool_alloc(subpool, 1024, NULL);
+    zt_ez_mempool_alloc(main_pool, 1024, NULL);
+
+    tdata = malloc(sizeof(struct test_data));
+    tdata->data1 = strdup("fjkdsl");
+    tdata->data2 = strdup("fjkdsl");
+    tdata->data3 = strdup("fjkdsl");
+    zt_ez_mempool_add_buffer(subpool, tdata, sizeof(*tdata), test_special_free);
 
     zt_ez_mempool_destroy(main_pool);
     return 0;
--- libzt/zt_ez_mempool.h
+++ libzt/zt_ez_mempool.h
@@ -3,11 +3,13 @@
 
 typedef struct zt_ez_mempool zt_ez_mempool;
 typedef struct zt_ez_mempool_chunk zt_ez_mempool_chunk;
+typedef void (*zt_ez_mempool_free_cb)(void *usrdata);
 
 struct zt_ez_mempool_chunk {
-    void                *data;
-    size_t               sz;
-    zt_ez_mempool_chunk *next;
+    void                 *data;
+    size_t                sz;
+    zt_ez_mempool_chunk   *next;
+    zt_ez_mempool_free_cb free_cb;
 };
 
 struct zt_ez_mempool {
@@ -28,9 +30,10 @@ zt_ez_mempool *zt_ez_mempool_init(zt_ez_mempool *parent);
  * Allocate memory from an ez memory pool
  * @param pool the memory pool to allocate from
  * @parem size the size to allocate
+ * @param an optional callback to free this data
  * @return a chunk of allocated memory
  */
-void *zt_ez_mempool_alloc(zt_ez_mempool *pool, size_t size);
+void *zt_ez_mempool_alloc(zt_ez_mempool *pool, size_t size, zt_ez_mempool_free_cb);
 
 /**
  * Destroy a memory pool (and all of its children)
@@ -44,8 +47,9 @@ void zt_ez_mempool_destroy(zt_ez_mempool *pool);
  * @param pool the memory pool
  * @param data the pointer to an allocated chunk of memory
  * @param size of the data
+ * @param an optional callback to free this data
  * @return 0 on success, -1 on error
  */
-int zt_ez_mempool_add_buffer(zt_ez_mempool *, void *, size_t);
+int zt_ez_mempool_add_buffer(zt_ez_mempool *, void *, size_t, zt_ez_mempool_free_cb);
 
 #endif
