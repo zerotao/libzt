13c5879feb10195b9ae4348b765777ec46cb11bf
Jason L. Shiffer <jshiffer@zerotao.org>
2011-07-19 13:41:25 -0400

    strict prototypes warning (missing void arg)

--- src/zt_log.c
+++ src/zt_log.c
@@ -41,7 +41,7 @@ static pthread_key_t log_key;
 static pthread_once_t log_key_once = PTHREAD_ONCE_INIT;
 
 static void
-make_logger_key()
+make_logger_key(void)
 {
     (void) pthread_key_create(&log_key, NULL);
 }
